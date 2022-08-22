22a1d7ba77750ac5de58875ff94fe41d10d87922
TaniCorn <63819551+TaniCorn@users.noreply.github.com>
2022-08-22 17:13:43 +0100

    Wave System Complete
    Waves completed. Plus ships speed up as the waves go on

--- Assets/scripts/manager.cs
+++ Assets/scripts/manager.cs
@@ -6,14 +6,15 @@ public class manager : MonoBehaviour
 {
 
     [SerializeField][Tooltip("Total cows that we currently have")]
-    private int totalCows;
+    private static int totalCows;
+    private static int totalShips;
     [SerializeField][Tooltip("Current wave")]
-    int wave;
+    int wave = 0;
 
     [SerializeField] private Vector3[] roamPoints;
     [SerializeField] private GameObject spaceship;
 
-    void RemoveCow()
+    public static void RemoveCow()
     {
         totalCows--;
         if (totalCows <= 0)
@@ -22,16 +23,23 @@ public class manager : MonoBehaviour
             Debug.Log("<color=green>GAME OVER</color>");
         }
     }
+    public void RemoveShip() {
+    
+        totalShips--;
+        if (totalShips <= 0)
+            StartCoroutine(StartWave());
+
+    }
 
     private void Start()
     {
         SpaceShip.SetRoamPoints(roamPoints);
         //Start first wave after x seconds(for exploration)
-
-
+        StartCoroutine(StartWave());
+        totalCows = FindObjectsOfType<Cow>().Length;
     }
 
-    IEnumerator StartFirstWave()
+    private IEnumerator StartWave()
     {
         yield return new WaitForSecondsRealtime(10.0f);
         StartNextWave();
@@ -40,10 +48,28 @@ public class manager : MonoBehaviour
     {
         wave++;
 
+        int amountOfSpaceships = DecideNumberOfSpaceships();
+        totalShips = amountOfSpaceships;
+        for (int i = 0; i < amountOfSpaceships; i++)
+        {
+            GameObject sp = Instantiate(spaceship, new Vector3(i * 30, 0, 0), Quaternion.identity, null);
+            sp.GetComponent<SpaceShip>().speed = wave;
+        }
         //Spawn alien spaceships
-        GameObject sp = Instantiate(spaceship, new Vector3(0,0,0), Quaternion.identity, null);
 
     }
 
-    
+    private int DecideNumberOfSpaceships()
+    {
+        if (wave > 10)
+        {
+            return totalCows;
+        }
+        int spa = totalCows - totalCows / wave;
+        if (spa < 0)
+        {
+            return 1;
+        }
+        return spa;
+    }
 }
